b2283549757556dcfd845cddf1bde89f9b097f4f
fbatlos <fbatlos123@g.educaand.es>
2024-09-24 18:14:47 +0200

    finish

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
 package org.example
-/*
 
-* */
+import org.example.repository.CotizacionRepository
+import java.nio.file.Path
+import kotlin.io.path.Path
+
+/*
+Nombre (nombre de la empresa), Final (precio de la acción al cierre de bolsa), Máximo (precio máximo de la acción durante la jornada), Mínimo (precio mínimo de la acción durante la jornada), Volumen (Volumen al cierre de bolsa), Efectivo (capitalización al cierre en miles de euros).
+ */
 
 fun main() {
+    val cotiza = CotizacionRepository()
+    val path = Path.of("src").resolve("main").resolve("ficheros")
+    cotiza.ShowAll(path).forEach { println(it)  }
+    cotiza.MakeSummary(path,cotiza.ShowAll(path))
 
 }
---
+++ src/main/kotlin/repository/CotizacionRepository.kt
@@ -0,0 +1,58 @@
+package org.example.repository
+
+import java.io.BufferedReader
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import kotlin.io.path.createFile
+import kotlin.io.path.notExists
+
+class CotizacionRepository {
+
+    fun ShowAll(paht:Path): MutableMap<Int,List<String>> {
+        val pahtFinal=paht.resolve("cotizacion.csv")
+        val br:BufferedReader = Files.newBufferedReader(pahtFinal)
+        br.readLine()
+        val informacionUnitaria = mutableMapOf<Int,List<String>>()
+        var numeros = 0
+
+        if (pahtFinal.notExists()){
+            pahtFinal.createFile()
+        }
+
+        br.use {
+            it.forEachLine { line ->
+                val allSpliteado = line.split(";")
+                informacionUnitaria[numeros]= allSpliteado
+                numeros++
+            }
+        }
+        return informacionUnitaria
+
+    }
+
+    fun MakeSummary(paht: Path,map: MutableMap< Int,List<String>>) {
+        val pahtFinal = paht.resolve("cotizacionFinal.csv")
+        if (pahtFinal.notExists()) {
+            pahtFinal.createFile()
+        }
+        var numero = 0
+        val bw = Files.newBufferedWriter(pahtFinal)
+
+        bw.use {
+                for (i in map) {
+                    val max = map.get(numero)?.get(2)?.replace(".", "")?.replace(",", ".")?.toDouble() ?: 0.0
+                    val min = map.get(numero)?.get(3)?.replace(".", "")?.replace(",", ".")?.toDouble() ?: 0.0
+                    val media = ((min + max) / 2).toString().format("%.2f").toDouble()
+                    it.write(
+                        "Name : ${
+                            map.get(numero)?.get(0)
+                        }  |  maximus : ${max}  |  minimum : ${min} |  average : ${media}\n"
+                    )
+
+                    numero++
+                }
+
+        }
+    }
+}
